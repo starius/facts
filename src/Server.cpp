0ce7ab4385ae0493e2ffe7f580ac2ea3dfe2b927
boris <bnagaev@gmail.com>
2011-08-05 03:48:26 +0400

    Server: addEntryPoint with Application

--- src/Server.cpp
+++ src/Server.cpp
@@ -7,15 +7,25 @@
  * See the LICENSE file for terms of use.
  */
 
+#include <boost/bind.hpp>
+
+#include <Wt/WEnvironment>
+
 #include "model/Fact.hpp"
 #include "Session.hpp"
+#include "Application.hpp"
 #include "config.hpp"
 
 namespace facts {
 
+Wt::WApplication* createApplication(Server* server, const Wt::WEnvironment& env) {
+    return new Application(env, *server);
+}
+
 Server::Server(int argc, char **argv):
     pool_(Session::new_connection(), CONNECTIONS_IN_POOL) {
     setServerConfiguration(argc, argv);
+    addEntryPoint(Wt::Application, boost::bind(createApplication, this, _1), "", "/favicon.ico");
     Session s(*this);
     s.reconsider();
 }
