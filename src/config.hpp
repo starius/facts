2508459f3d80e754ed1090db7a29e27e1ce401b5
boris <bnagaev@gmail.com>
2011-08-05 02:07:47 +0400

    add config

---
+++ src/config.hpp
@@ -0,0 +1,23 @@
+/*
+ * facts, random fact viewer
+ * Copyright (C) 2010 Boris Nagaev
+ *
+ * facts is licensed under the GNU GPL Version 2.
+ * Other versions of the GPL do not apply.
+ * See the LICENSE file for terms of use.
+ */
+
+#ifndef FACTS_CONFIG_HPP_
+#define FACTS_CONFIG_HPP_
+
+/** \file Compile-time constants */
+
+namespace facts {
+
+const int CONNECTIONS_IN_POOL = 10;
+const char* const SQLITE_DATABASE_NAME = "facts.db";
+
+}
+
+#endif
+
